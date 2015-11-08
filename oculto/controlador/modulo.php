cd914898895e25acac62688e4531c1cc78d5fea3
josecarlosqs <josec.quichiz@gmail.com>
2015-11-07 23:19:34 -0500

    Añadido referencia a archivos con dirname

--- oculto/controlador/modulo.php
+++ oculto/controlador/modulo.php
@@ -7,13 +7,13 @@ class Modulo{
 		$this->tituloModulo = "";
 		$this->contenidoModulo = "";
 		if(preg_match("/^[a-zA-z0-9\-]+$/", $nomArchivoModulo)){
-			if(file_exists('../pantallas/'.$nomArchivoModulo.'.php')){
+			if(file_exists(dirname(__FILE__).'/../pantallas/'.$nomArchivoModulo.'.php')){
 				include dirname(__FILE__).'/../pantallas/'.$nomArchivoModulo.'.php';
 			}else{
 				include dirname(__FILE__).'/../pantallas/404.php';
 			}
 		}else{
-			include dirname(__FILE__).'/../../pantallas/399.php';
+			include dirname(__FILE__).'/../pantallas/399.php';
 		}
 		$this->tituloModulo = $datosModulo['titulo'];
 		$this->contenidoModulo = $datosModulo['contenido'];
